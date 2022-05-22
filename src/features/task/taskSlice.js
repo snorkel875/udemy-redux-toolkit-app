fe08a345524fe2cb2ac72c48700f9072abae43e0
snorkel875 <mianqiangyong71@gmail.com>
2022-05-22 20:22:43 +0900

    [add]タスク一覧の表示機能を実装

---
+++ src/features/task/taskSlice.js
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  idCount: 3,
+  tasks: [
+    {
+      id: 3,
+      title: "TASK C",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "TASK B",
+      completed: true,
+    },
+    {
+      id: 1,
+      title: "TASK A",
+      completed: false,
+    }
+  ]
+};
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {
+    newTask: (state, action) => {
+      state.idCount += 1;
+      const newItem = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      };
+      state.tasks = [newItem, ...state.tasks];
+    },
+    completeTask: (state, action) => {
+      const task = state.tasks.find((t) => t.id === action.payload.id);
+      if (task) {
+        task.completed = !task.completed;
+      }
+    },
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((t) => t.id !== action.payload.id);
+    },
+  },
+});
+
+export const { newTask, completeTask, deleteTask } = taskSlice.actions;
+
+export const selectTasks = (state) => state.task.tasks;
+
+export default taskSlice.reducer;
